9c9b2aa8721dbd0c6937d94972afac2484f30c1b
root <root@localhost.localdomain>
2018-05-12 01:27:26 +0900

    style:modify directory constitution

---
+++ Api/v1/app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HelloWorldController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function show()
+    {
+        return 'hello world';
+    }
+
+    //
+}
