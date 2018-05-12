0c41919719795c8adadb1398a0cdcf59d6aec697
ponpu1601 <2ndrelay.pumpstation@gmai.com>
2018-05-12 15:33:26 +0900

    style:インデント調整

--- Api/app/Http/Controllers/ProgramController.php
+++ Api/app/Http/Controllers/ProgramController.php
@@ -19,42 +19,43 @@ class ProgramController extends Controller
     public function show(Request $request)
     {
         if ($request->has('title'))
-         {
+        {
             $programs=$this->fetchByParticalMatchTitle($request->input('title'));
             return $this->respondWithJson($programs);
-         }
-	 else
-         {
+        }
+	    else
+        {
             $programs=$this->fetchRecentPrograms();
             return $this->respondWithJson($programs);
-         }
+        }
     }
 
     public function fetchRecentPrograms()
     {
         return Program::recent()
-                   ->with('program_type','game_genre')
-                   ->get();
+                ->with('program_type','game_genre')
+                ->get();
     }
 
     public function fetchByParticalMatchTitle($title)
     {
         return Program::with('program_type','game_genre')
-                   ->where(function($query) use ($title)
-                     {
-                       $query->orWhere('title','LIKE',"%$title%")
-                             ->orWhere('kana_title','LIKE',"%$title%")
-                             ->orWhere('other_title_01','LIKE',"%$title%")
-                             ->orWhere('other_title_02','LIKE',"%$title%");
-                   })->get(); 
+                ->where(function($query) use ($title)
+                {
+                    $query->orWhere('title','LIKE',"%$title%")
+                          ->orWhere('kana_title','LIKE',"%$title%")
+                          ->orWhere('other_title_01','LIKE',"%$title%")
+                          ->orWhere('other_title_02','LIKE',"%$title%");
+                })
+                ->get(); 
     }
 
     public function showOne($id)
     {
         $program=Program::with('program_type','game_genre','songs')
-                   ->where('id',$id)
-                   ->get();
-
+                  ->where('id',$id)
+                  ->get();
+                  
         return $this->respondWithJson($program);
     }
     
