3b72a783edfb55b960bff97a036bce5045315156
ponpu1601 <2ndrelay.pumpstation@gmai.com>
2018-05-12 15:22:20 +0900

    feat:楽曲データはsong_role,singerも同時に取得する style:インデント調整 Signed-off-by: ponpu1601 <2ndrelay.pumpstation@gmai.com>

--- Api/app/Program.php
+++ Api/app/Program.php
@@ -17,23 +17,23 @@ class Program extends Model
 
     public function scopeRecent($query)
     {
-	return $query->orderBy('anisoninfo_program_id','desc')
+        return $query->orderBy('anisoninfo_program_id','desc')
                     ->take(200);
     }
 
     public function game_genre()
     {
-	return $this->belongsTo('App\GameGenre','game_genre_id','id');
+        return $this->belongsTo('App\GameGenre','game_genre_id','id');
     }
 
     public function program_type()
     {
-	return $this->belongsTo('App\ProgramType','program_type_id','id');
+    	return $this->belongsTo('App\ProgramType','program_type_id','id');
     }
     
     public function songs()
     {
-	return $this->hasMany('App\Song','program_id','id');
+	    return $this->hasMany('App\Song','program_id','id')->with('song_role','singer');
     }
 
 }
